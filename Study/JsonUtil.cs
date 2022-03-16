fb30fd102eb1d2ed6db9de8d350682ae2a6641d1
inazor <igor.nazor@gmail.com>
2022-03-16 15:54:19 +0100

    replace jsondataacess with jsonutil

---
+++ Study/JsonUtil.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Study
+{
+    public static class JsonUtil
+    {
+        public static List<T> ReadJsonFile<T>()
+        {
+            var fileName = Path.Combine(Config.RootDirectory, "Study", "DataAccess", "JsonData", $"{typeof(T).Name}.json");
+
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string json = reader.ReadToEnd();
+                List<T> items = JsonConvert.DeserializeObject<List<T>>(json);
+                return items;
+            }
+        }
+    }
+}
