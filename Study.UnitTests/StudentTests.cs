3e46c3197681f1b1c02cbcfa1dfff5960cc5f47a
inazor <igor.nazor@gmail.com>
2022-04-08 12:23:18 +0200

    vj3.1 final

---
+++ Study.UnitTests/StudentTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using Study.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.UnitTests
+{
+    [TestFixture]
+    public class StudentTests
+    {
+        private Student _student;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _student = new Student()
+            {
+                Name = "Marko",
+                Level = 5
+            };
+        }
+
+
+        [Test]
+        [TestCase(1, 2, true)]
+        [TestCase(2, 2, false)]
+        [TestCase(3, 2, false)]
+        [TestCase(1, 3, false)]
+        public void CanEnrollToCourse_WhenCalled_ReturnIfStudentCanEnrollToCourse(int studentLevel, int courseLevel, bool expected)
+        {
+            _student.Level = studentLevel;
+
+            var result = _student.CanEnrollToCourse(courseLevel);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+        [Test]
+        public void Introduce_WhenCalled_ReturnExactIntroductionString()
+        {
+            var result = _student.Introduce();
+            Assert.That(result, Is.EqualTo("Hello! My name is Marko and my knowledge level is 5"));
+        }
+
+        [Test]
+        public void Introduce_WhenCalled_ReturnStringContainingHelloAndStudentName()
+        {
+            var result = _student.Introduce();
+            Assert.That(result, Does.Contain("marko").IgnoreCase.And.Contain("hello").IgnoreCase);
+        }
+    }
+}
---
+++ Study.UnitTests/UtilTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.UnitTests
+{
+    [TestFixture]
+    public class UtilTests
+    {
+        [Test]
+        public void Add_OneAndOne_ReturnTwo()
+        {
+            var a = 1;
+            var b = 1;
+
+            var result = Util.Add(a, b);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Add_TwoAndThree_ReturnFive()
+        {
+            var a = 2;
+            var b = 3;
+
+            var result = Util.Add(a, b);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Max_TwoAndThree_ReturnThree()
+        {
+            var a = 2;
+            var b = 3;
+
+            var result = Util.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_ThreeAndTwo_ReturnThree()
+        {
+            var a = 3;
+            var b = 2;
+
+            var result = Util.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(1, 1, 2)]
+        [TestCase(2, 3, 5)]
+        public void Add_TwoIntegers_ReturnSum(int a, int b, int expected)
+        {
+            var result = Util.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(2, 3, 3)]
+        [TestCase(3, 2, 3)]
+        public void Max_TwoIntegers_ReturnTheLargerNumber(int a, int b, int expected)
+        {
+            var result = Util.Max(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+    }
+}
