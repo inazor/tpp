caa3a8c027f89bbbc95efd11511a837f379f7c89
inazor <igor.nazor@gmail.com>
2022-05-04 15:49:41 +0200

    add city

---
+++ Study/Controllers/CityController.cs
@@ -0,0 +1,29 @@
+﻿using Study.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Study.Controllers
+{
+    public class CityController : Controller
+    {
+        public CityRepository CityRepository = new CityRepository(Config.DataAccess);
+
+        public ActionResult List()
+        {
+            var cities = CityRepository.GetAll();
+            ViewBag.Cities = cities;
+            return View();
+        }
+
+        public ActionResult Card(int id)
+        {
+            var city = CityRepository.GetById(id);
+            ViewBag.City = city;
+
+            return View();
+        }
+    }
+}
---
+++ Study/DataAccess/Repositories/CityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Study.Models;
+using Study.Persistence;
+using Study.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Study.DataAccess.Repositories
+{
+    public class CityRepository : Repository<City>
+    {
+        public CityRepository(IDataAccess dataAccess) : base(dataAccess)
+        {
+
+        }
+    }
+}
