3b7bd9f0a6555f42790754b228c5da49837d2418
inazor <igor.nazor@gmail.com>
2022-05-05 13:01:53 +0200

    student tests

--- Study.UnitTests/StudentTests.cs
+++ Study.UnitTests/StudentTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using Study.Core.Repositories;
 using Study.Models;
 using System;
 using System.Collections.Generic;
@@ -6,50 +8,126 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Study.UnitTests
+namespace Study.UnitTests.Core.Models
 {
     [TestFixture]
     public class StudentTests
     {
-        private Student _student;
+        private Mock<IStudentRepository> _studentRepository;
+        private Course _course;
+        private Student _sut;
 
         [SetUp]
         public void SetUp()
         {
-            _student = new Student()
+            _studentRepository = new Mock<IStudentRepository>();
+            _course = new Course
             {
-                Name = "Marko",
-                Level = 5
+                Id = 1,
+                Name = "Specijalistički računarstvo",
+                Level = 6
+            };
+            var studentsWithCourses = new List<Student>()
+            {
+                new Student()
+                {
+                    Name = "Jure",
+                    Level = 1,
+                    Course = _course,
+                    CourseId = _course.Id
+                },
+                new Student()
+                {
+                    Name = "Ante",
+                    Level = 1,
+                    Course = _course,
+                    CourseId = _course.Id
+                }
+            };
+            _studentRepository.Setup(repo => repo.GetStudentsWithCourses()).Returns(studentsWithCourses);
+            _sut = new Student(_studentRepository.Object)
+            {
+                Name = "Mate",
+                Level = 1,
+                Course = _course,
+                CourseId = _course.Id
             };
         }
 
+        [Test]
+        public void CanEnrollToCourse_CourseLevelIsGreaterByOneThanStudentLevel_ReturnTrue()
+        {
+            var courseLevel = 2;
+
+            var result = _sut.CanEnrollToCourse(courseLevel);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanEnrollToCourse_CourseLevelIsTheSameAsStudentLevel_ReturnFalse()
+        {
+            _sut.Level = 2;
+            var courseLevel = 2;
+            var result = _sut.CanEnrollToCourse(courseLevel);
+            Assert.That(result, Is.False);
+        }
 
         [Test]
         [TestCase(1, 2, true)]
-        [TestCase(2, 2, false)]
-        [TestCase(3, 2, false)]
+        [TestCase(1, 1, false)]
         [TestCase(1, 3, false)]
-        public void CanEnrollToCourse_WhenCalled_ReturnIfStudentCanEnrollToCourse(int studentLevel, int courseLevel, bool expected)
+        [TestCase(3, 1, false)]
+        public void CanEnrollToCourse_WhenCalled_ReturnsCorrectValue(int studentLevel, int courseLevel, bool expectedValue)
         {
-            _student.Level = studentLevel;
+            _sut.Level = studentLevel;
 
-            var result = _student.CanEnrollToCourse(courseLevel);
-            Assert.That(result, Is.EqualTo(expected));
+            var result = _sut.CanEnrollToCourse(courseLevel);
+            Assert.That(result, Is.EqualTo(expectedValue));
         }
 
-
+        // Specific Fragile test
         [Test]
-        public void Introduce_WhenCalled_ReturnExactIntroductionString()
+        public void Introduce_WhenCalled_ReturnsIntroductionString()
         {
-            var result = _student.Introduce();
-            Assert.That(result, Is.EqualTo("Hello. My name is Marko and my knowledge level is 5"));
+            var result = _sut.Introduce();
+            Assert.That(result, Is.EqualTo("Hello. My name is Mate and my knowledge level is 1"));
+        }
+
+        // General test
+        [Test]
+        public void Introduce_WhenCalled_ReturnsStringContainingNameAndLevel()
+        {
+            var result = _sut.Introduce();
+            Assert.That(result, Does.Contain("mate").IgnoreCase.And.Contain("1"));
         }
 
         [Test]
-        public void Introduce_WhenCalled_ReturnStringContainingHelloAndStudentName()
+        public void Enroll_StudentLevelIsCorrect_CallUpdate()
         {
-            var result = _student.Introduce();
-            Assert.That(result, Does.Contain("marko").IgnoreCase.And.Contain("hello").IgnoreCase);
+            _sut.Level = 5;
+            _course.Level = 6;
+
+            _sut.Enroll(_course);
+            _studentRepository.Verify(repo => repo.Update(It.IsAny<int>(), It.IsAny<Student>()));
+        }
+
+        [Test]
+        [TestCase(1, 1)]
+        [TestCase(1, 3)]
+        [TestCase(2, 1)]
+        [TestCase(5, 2)]
+        public void Enroll_StudentLevelIsIncorrect_ThrowArgumentException(int studentLevel, int courseLevel)
+        {
+            _sut.Level = studentLevel;
+            _course.Level = courseLevel;
+
+            Assert.That(() => _sut.Enroll(_course), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void GetNumberOfCourseMates_WhenCalled_ReturnNumberOfCourseMates()
+        {
+            Assert.That(_sut.GetNumberOfCourseMates(), Is.EqualTo(2));
         }
     }
 }
