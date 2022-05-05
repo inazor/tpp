0e457bed07b0ccb7e7128b4917f798a6865006bc
inazor <igor.nazor@gmail.com>
2022-05-05 13:39:11 +0200

    student repo tests

---
+++ Study.UnitTests/DataAccess/Repositories/StudentRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using Study.Models;
+using Study.Persistence;
+using Study.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.UnitTests.DataAccess.Repositories
+{
+    [TestFixture]
+    public class StudentRepositoryTests
+    {
+        [Test]
+        public void GetStudentWithCourse_WhenCalled_CallsGetById()
+        {
+            var dataAccess = new Mock<IDataAccess>();
+            dataAccess.Setup(da => da.GetById<Student>(It.IsAny<int>())).Returns(new Student() { CourseId = 5 });
+            dataAccess.Setup(da => da.GetById<Course>(5)).Returns(new Course() { Id = 5 });
+
+            var sut = new StudentRepository(dataAccess.Object);
+
+            var result = sut.GetStudentWithCourse(1);
+
+            dataAccess.Verify(da => da.GetById<Student>(It.IsAny<int>()));
+            dataAccess.Verify(da => da.GetById<Course>(It.IsAny<int>()));
+        }
+    }
+}
