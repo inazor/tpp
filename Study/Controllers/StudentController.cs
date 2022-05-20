760bbd99409bd6cf75c6404ceefc30ecd3953a07
inazor <igor.nazor@gmail.com>
2022-05-20 10:33:44 +0200

    add CRUD to student view

--- Study/Controllers/StudentController.cs
+++ Study/Controllers/StudentController.cs
@@ -1,3 +1,5 @@
+using Study.Core.Repositories;
+using Study.Models;
 using Study.Persistence;
 using Study.Persistence.Repositories;
 using System;
@@ -10,35 +12,69 @@ namespace Study.Controllers
 {
     public class StudentController : Controller
     {
-        private StudentRepository studentRepository = new StudentRepository(Config.DataAccess);
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public StudentController()
+        {
+            _studentRepository = new StudentRepository(Config.DataAccess);
+        }
 
         public ActionResult Delete(int id)
         {
-            studentRepository.Remove(id);
+            _studentRepository.Remove(id);
 
-            var students = studentRepository.GetAll();
+            var students = _studentRepository.GetAll();
             ViewBag.Students = students;
             return View("List");
         }
 
         public ActionResult List()
         {
-            var students = studentRepository.GetAll();
+            var students = _studentRepository.GetAll();
             ViewBag.Students = students;
             return View();
         }
 
-        public ActionResult Card(int id)
+        public ActionResult Card(int? id)
         {
-            var student = studentRepository.GetStudentWithCourseAndCity(id);
+            if (id == null)
+            {
+                ViewBag.Student = new Student();
+                ViewBag.CourseName = "";
+                ViewBag.CityName = "";
+                return View();
+            }
+
+            var student = _studentRepository.GetStudentWithCourseAndCity((int)id);
             ViewBag.Student = student;
 
             ViewBag.CourseId = student.Course?.Id;
             ViewBag.CourseName = student.Course?.Name ?? "";
-
             ViewBag.CityName = student.City?.Name ?? "";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Student student)
+        {
+            if (student.Id == 0)
+            {
+                _studentRepository.Add(student);
+            }
+            else
+            {
+                _studentRepository.Update(student.Id, student);
+            }
+
+            var students = _studentRepository.GetAll();
+            ViewBag.Students = students;
+            return View("List");
         }
     }
 }
