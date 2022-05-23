43a0214ca113522b08b9518abfbf454ca6d04f84
inazor <igor.nazor@gmail.com>
2022-05-23 09:09:50 +0200

    add country repository

---
+++ Study.Core/DataAccess/Repositories/CountryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Study.DataAccess.Interfaces;
+using Study.Models;
+using Study.Persistence;
+using Study.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Core.DataAccess.Repositories
+{
+    public class CountryRepository : Repository<Country>, ICountryRepository
+    {
+        public CountryRepository(IDataAccess dataAccess) : base(dataAccess)
+        {
+
+        }
+    }
+}
