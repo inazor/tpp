c599eba79547ee8e588298562dc6e165764e8861
inazor <igor.nazor@gmail.com>
2022-05-23 09:15:33 +0200

    test with country repo

--- Study.IntegrationTests/Controllers/HomeControllerTests.cs
+++ Study.IntegrationTests/Controllers/HomeControllerTests.cs
@@ -18,7 +18,7 @@ namespace Study.IntegrationTests.Controllers
     public class HomeControllerTests
     {
         [Test]
-        public void Index_WhenCalled_FillViewBagWithData()
+        public void Index_WithStub_FillViewBagWithData()
         {
             var countryRepositoryStub = new Mock<ICountryRepository>();
             countryRepositoryStub.Setup(repo => repo.GetAll()).Returns(new List<Country> { 
@@ -40,6 +40,23 @@ namespace Study.IntegrationTests.Controllers
                 new CityRepository(new SqliteDataAccess()),
                 countryRepositoryStub.Object);
 
+
+            sut.Index();
+
+            Assert.That(sut.ViewBag.Students.Count, Is.GreaterThan(0));
+            Assert.That(sut.ViewBag.Courses.Count, Is.GreaterThan(0));
+            Assert.That(sut.ViewBag.Cities.Count, Is.GreaterThan(0));
+            Assert.That(sut.ViewBag.Countries.Count, Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void Index_WithImplementation_FillViewBagWithData()
+        {
+            var sut = new HomeController(
+                new StudentRepository(new SqliteDataAccess()),
+                new CourseRepository(new SqliteDataAccess()),
+                new CityRepository(new SqliteDataAccess()),
+                new CountryRepository(new SqliteDataAccess()));
 
             sut.Index();
 
